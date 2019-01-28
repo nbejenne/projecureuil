673daf522ab9d7c85529ae8a800b741a8be78aca
Nicolas Bejenne <nicolas.bejenne@gmail.com>
2019-01-28 19:44:32 +0100

    Routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'albums#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :albums do
+    resources :invitations, only: [:create, :index]
+    resources :pictures, only: [:new, :create, :show, :destroy], shallow: true do
+      resources :reactions, only: [:create, :destroy]
+    end
+  end
 end
