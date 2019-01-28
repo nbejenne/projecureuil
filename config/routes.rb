9a823028e38e157584fff311c25595046605414e
Nicolas Bejenne <nicolas.bejenne@gmail.com>
2019-01-28 22:34:09 +0100

    generated controller album and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'albums/index'
+  get 'albums/new'
+  get 'albums/create'
+  get 'albums/edit'
+  get 'albums/update'
+  get 'albums/destroy'
   devise_for :users
   root to: 'albums#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
